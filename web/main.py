2ec8870ad6381f921852594b0abc95b6eba3e415
jhuizy <jhuizenga99@gmail.com>
2019-06-14 14:29:45 +0800

    added names endpoint

--- web/main.py
+++ web/main.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 
 connection_str = "dbname='postgres' user='postgres' password='postgres' host='db'"
 
+@app.route("/items/names")
+def names():
+  try:
+    con = psycopg2.connect(connection_str)
+    cur = con.cursor()
+    items = get_names(cur)
+    return jsonify(items)
+  except Exception as e:
+    print("Error: %s" % e)
+  finally:
+    con.close()
+
 @app.route("/items")
 def index():
   try:
@@ -35,6 +47,10 @@ def get_history(name):
     print("Error: %s" % e)
   finally:
     con.close()
+
+def get_names(cur):
+  cur.execute("SELECT name FROM items i1 WHERE i1.overall_quantity > 0 AND i1.overall_average > 0 AND i1.sell_quantity > 0 AND i1.sell_average > 0 AND i1.buy_quantity > 0 AND buy_average > 0 AND i1.time = (select max(i2.time) from items i2 where i1.id = i2.id) order by name")
+  return [i[0] for i in cur.fetchall()]
 
 def get_all_items(cur, page=0, per_page=20):
   cur.execute("SELECT i1.time, i1.name, i1.members, i1.overall_quantity, i1.overall_average, i1.sell_quantity, i1.sell_average, i1.buy_quantity, buy_average FROM items i1 WHERE i1.overall_quantity > 0 AND i1.overall_average > 0 AND i1.sell_quantity > 0 AND i1.sell_average > 0 AND i1.buy_quantity > 0 AND buy_average > 0 AND i1.time = (select max(i2.time) from items i2 where i1.id = i2.id) order by name LIMIT %s OFFSET %s", (per_page, page * per_page,))
