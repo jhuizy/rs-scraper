aff8db41375378abd0dfdf33008a5ecf803de47f
jhuizy <jhuizenga99@gmail.com>
2019-06-20 12:34:41 +0800

    fixed some frontend issues

--- frontend/src/Unicorns.js
+++ frontend/src/Unicorns.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Label, Menu, Table, Dimmer, Loader } from 'semantic-ui-react'
 
 const loadItems = (onLoaded) => {
   fetch("http://ge-tracker.jhuizy.com:3001/unicorns")
@@ -13,16 +13,16 @@ const formatMoney = (gp) => {
     const M_10 = 10000000
     const K_10 = 10000
     if (gp > M_10) {
-      return `${(gp / M_10).toFixed(2)}M`    
+      return `${(gp / M_10).toFixed(2)}M`
     } else if (gp > K_10) {
       return `${(gp / K_10).toFixed(2)}K`
     } else {
       return gp
     }
   }
-  
+
   return `${gp} (${rounded(gp)})`
-  
+
 }
 
 const formatPercent = (perc) => {
@@ -32,25 +32,30 @@ const formatPercent = (perc) => {
 export default () => {
 
   const [items, setItems] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
 
   React.useEffect(() => {
+    setLoading(true)
     if (items == null) {
-      loadItems(setItems)
+      loadItems(items => {
+        setItems(items)
+        setLoading(false)
+      })
     }
-  }, [items])
-  
+  }, [items, loading])
+
   const renderRow = (item) => (
-        <Table.Row>
-          <Table.Cell>{item['name']}</Table.Cell>
-          <Table.Cell>{formatMoney(item['buy'])}</Table.Cell>
-          <Table.Cell>{formatMoney(item['sell'])}</Table.Cell>
-          <Table.Cell>{formatPercent(item['margin_percent'])}</Table.Cell>
-          <Table.Cell>{formatMoney(item['margin_gp'])}</Table.Cell>
-          <Table.Cell>{item['score']}</Table.Cell>
-        </Table.Row>
+    <Table.Row>
+      <Table.Cell>{item['name']}</Table.Cell>
+      <Table.Cell>{formatMoney(item['buy'])}</Table.Cell>
+      <Table.Cell>{formatMoney(item['sell'])}</Table.Cell>
+      <Table.Cell>{formatPercent(item['margin_percent'])}</Table.Cell>
+      <Table.Cell>{formatMoney(item['margin_gp'])}</Table.Cell>
+      <Table.Cell>{item['score']}</Table.Cell>
+    </Table.Row>
   )
 
-  return (
+  const renderTable = () => (
     <Table celled>
       <Table.Header>
         <Table.Row>
@@ -67,5 +72,18 @@ export default () => {
         {items && items.map(renderRow)}
       </Table.Body>
     </Table>
+  )
+
+  const renderLoading = () => (
+    <Dimmer active>
+      <Loader>Loading</Loader>
+    </Dimmer>
+  )
+
+  return (
+    <div>
+      {loading && renderLoading()}
+      {!loading && renderTable()}
+    </div>
   )
 }
